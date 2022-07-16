01349743b4a3c697b7e4d6d0dcf948a6c7e513b8
mikun <45915246+mikunNEM@users.noreply.github.com>
2022-07-16 17:02:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ accountHttp.getAccountInfo(address)
     }
   })
   
+  console.log(address);
                                                  // トランザクション履歴を取得する
 const searchCriteria = {                                   
   group: symbol.TransactionGroup.Confirmed,
@@ -35,7 +36,6 @@ const searchCriteria = {
   pageNumber: 1,
   pageSize: 10,
   order: symbol.Order.Desc,
-  console.log(address);
 }
 
 transactionHttp
